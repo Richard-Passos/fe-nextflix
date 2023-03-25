317769d53216aa90190eb7ac6f2937d02a5d738a
Richard passos <richard.ac.passos12@gmail.com>
2023-03-25 15:50:44 -0300

    deploy test on vercel

--- src/components/main-page/MainPage.jsx
+++ src/components/main-page/MainPage.jsx
@@ -7,7 +7,6 @@ export default function MainPage({ popularM, popularS }) {
   return (
     <Container>
       {popularM && popularM.map((movie) => <p key={movie.id}>{movie.title}</p>)}
-      <Container />
       {popularS && popularS.map((serie) => <p key={serie.id}>{serie.name}</p>)}
       <Movies classification="top_rated" />
       <Series classification="top_rated" />
